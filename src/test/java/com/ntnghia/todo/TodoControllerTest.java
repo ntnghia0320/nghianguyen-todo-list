986242b5aa4fa0ff2143ecc3199e5e750c93b7c8
Nghia Nguyen <ntnghia0320@gmail.com>
2021-07-01 16:38:23 +0700

    Update test controller

--- src/test/java/com/ntnghia/todo/TodoControllerTest.java
+++ src/test/java/com/ntnghia/todo/TodoControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @WebAppConfiguration
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class TodoControllerTest {
     @Autowired
     private TodoRepository todoRepository;
