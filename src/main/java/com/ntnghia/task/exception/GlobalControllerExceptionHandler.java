a0de3a74c5c3a020e7fd934668640ea156308edc
ntnghia0320 <ntnghia0320@gmail.com>
2021-07-05 15:51:29 +0700

    Fix exception.

--- src/main/java/com/ntnghia/task/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/ntnghia/task/exception/GlobalControllerExceptionHandler.java
@@ -3,11 +3,12 @@ package com.ntnghia.task.exception;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Data
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
@@ -16,9 +17,10 @@ class ErrorModel {
     private String path;
 }
 
-@ControllerAdvice
-@ConditionalOnProperty(prefix = "app", name = "disable-default-exception-handling")
-public class GlobalControllerExceptionHandler {
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorModel> handleException(NotFoundException ex, ServletWebRequest request) {
         return new ResponseEntity<>(new ErrorModel(ex.getMessage(), request.getRequest().getRequestURI()), HttpStatus.NOT_FOUND);
     }
